17b281311aca616b5d226fc9e778c6369a7a5789
Jeffrey Kao <jeffthestampede@gmail.com>
2013-04-19 18:01:02 -0700

    removed json from pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,24 +4,23 @@ class PagesController < ApplicationController
 
   def results
    
-    @team1 = (Team.where(:name => "#{current_user.team1}").as_json)[0]
-    @team2 = (Team.where(:name => "#{current_user.team2}").as_json)[0]
-    @team3 = (Team.where(:name => "#{current_user.team3}").as_json)[0]
+    @team1 = (Team.where(:name => "#{current_user.team1}")).first
+    @team2 = (Team.where(:name => "#{current_user.team2}")).first
+    @team3 = (Team.where(:name => "#{current_user.team3}")).first
 
-    
-
-    file = open("http://api.espn.com/v1/sports/#{@team1["sport"]}/#{@team1["league"]}/teams/#{@team1["espn_id"]}/news?apikey=mq6g97278vj2y36a2c4kdheb")
+    file = open("http://api.espn.com/v1/sports/#{@team1.sport}/#{@team1.league}/teams/#{@team1.espn_id}/news?apikey=mq6g97278vj2y36a2c4kdheb")
     @results = JSON.load(file.read)["headlines"]
 
-    file2 = open("http://api.espn.com/v1/sports/#{@team2["sport"]}/#{@team2["league"]}/teams/#{@team2["espn_id"]}/news?apikey=mq6g97278vj2y36a2c4kdheb")
+    file2 = open("http://api.espn.com/v1/sports/#{@team2.sport}/#{@team2.league}/teams/#{@team2.espn_id}/news?apikey=mq6g97278vj2y36a2c4kdheb")
     @results2 = JSON.load(file2.read)["headlines"]
 
-    file3 = open("http://api.espn.com/v1/sports/#{@team3["sport"]}/#{@team3["league"]}/teams/#{@team3["espn_id"]}/news?apikey=mq6g97278vj2y36a2c4kdheb")
+    file3 = open("http://api.espn.com/v1/sports/#{@team3.sport}/#{@team3.league}/teams/#{@team3.espn_id}/news?apikey=mq6g97278vj2y36a2c4kdheb")
     @results3 = JSON.load(file3.read)["headlines"]
 
   end
 
   def index
+
 
   end
 
