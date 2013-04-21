4e59103b44b4a57c3d7f25a75d67caae1c1c6256
Jeffrey Kao <jeffthestampede@gmail.com>
2013-04-20 18:53:40 -0700

    add twitter search results

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require 'rails/all'
 
 require 'open-uri'
 
+require 'uri'
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
